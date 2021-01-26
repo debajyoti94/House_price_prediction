54bc0f86a6331a1600442f3f7e69ecf3bbf282cc
debajyoti <debajyoti94@gmail.com>
2021-01-26 19:58:29 +0530

    Added code for abc, feature_engg class and dump_load pickled files.

--- src/feature_engg.py
+++ src/feature_engg.py
@@ -2,20 +2,75 @@
 
 #  import modules here
 import pandas as pd
-
+from sklearn.preprocessing import MinMaxScaler
+import abc
+import pickle
 
 # create abc class here
+class MustHaveForFeatureEngg:
 
+    __metaclass__ = abc.ABCMeta
 
+    @abc.abstractmethod
+    def drop_features(self):
+        '''
+        Pass a list of features that you want to drop
+        :return: dataframe with the dropped features
+        '''
+        return
+
+    @abc.abstractmethod
+    def cleaning_data(self):
+        '''
+        Clean the given input data
+        :return: cleaned data
+        '''
+        return
 
 
 # create feature engg class here
+class FeatureEngg(MustHaveForFeatureEngg):
+
+    def cleaning_data(self, data, dataset_type):
+        '''
+
+        :param data:
+        :param dataset_type:
+        :return:
+        '''
+        return
 
 
-    # functions here
+    def drop_features(self, data_df, features_to_drop):
+        '''
+        Function to drop the features that are passed
+         in the parameter
+        :param data_df: The input dataframe
+        :param features_to_drop: list of features that you want to drop
+        :return: dataframe with dropped features
+        '''
 
+# class for dumping and loading pickled files
+class DumpLoadFile:
 
+    def load_file(self, filename):
+        """
+        For loading the pickled files
+        :param filename: the file that you want to load
+        :return: the loaded file
+        """
+        with open(filename, "rb") as pickle_handle:
+            return pickle.load(pickle_handle)
 
+    def dump_file(self, filename, *file):
+        """
+        Pickle a file
+        :param *file: files that you want to pickle
+        :param filename: filename for the pickled file
+        :return: nothing
+        """
+        with open(filename, "wb") as pickle_handle:
+            pickle.dump(file, pickle_handle)
 
 
 # create pickle dump load class here
